19815b970bdec2f8bd40b2575f80b9baf55fdd4a
Deependra Yadav <56085092+deependrayadav@users.noreply.github.com>
2021-07-23 17:56:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def generate_frames():
    if not success:
        break
    else:
-       detector=cv2.CascadeClassifier('opencv\data\haarcascades\haarcascade_frontalface_default.xml')
-       eye_cascade=cv2.CascadeClassifier('opencv\data\haarcascades\haarcascade_eye.xml')
+       detector=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+       eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
        faces=detector.detectMultiScale(frame,1.1,7)
        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
        for(x,y,w,h) in faces:
